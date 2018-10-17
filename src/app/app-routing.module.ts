731827561fa1c6e27c8371ebd0dde61aa2a8faa6
Oleg Shmuelov <oleg.shmuelov@gmail.com>
2018-10-17 11:28:56 +0300

    add ticker route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { TickersComponent } from './tickers/tickers.component'
+import { TickerComponent } from './ticker/ticker.component';
 
 
 const routes: Routes = [
 
-  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: '', redirectTo: '/tickers', pathMatch: 'full' },
   { path: 'dashboard', component: DashboardComponent },
+  { path: 'ticker/:symbol', component: TickerComponent },
+  { path: 'tickers', component: TickersComponent },
 
   // otherwise redirect to home
   { path: '**', redirectTo: '' }
